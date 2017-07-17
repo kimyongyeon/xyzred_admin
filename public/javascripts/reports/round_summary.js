cc665d3bfcff067fd9543413905957a58545a257
x8tech <x8tech@gmail.com>
2017-07-18 00:54:28 +0900

    edit

--- public/javascripts/reports/round_summary.js
+++ public/javascripts/reports/round_summary.js
@@ -22,15 +22,27 @@ RoundSummary.prototype.list = function(searchName) {
     .done(function( msg ) {
         var list = $("#round_list > tbody");
         var html = "";
+        var margin_sum = 0;
         for (var i=0; i<msg.length; i++) {
+
+            var bet = Math.floor(msg[i].bet);
+            var cash_out = Math.floor(msg[i].cash_out == null ? 0 :  msg[i].cash_out   );
+            var bonus = Math.floor(msg[i].bonus == null ? 0 : msg[i].bonus);
+            var margin = bet - cash_out - bonus;
+            margin_sum = margin_sum + margin;
+            console.log("margin_sum",margin_sum, margin);
+
             html += "<tr>" +
                 "<td>"+ msg[i].game_id+ "</td>" +
-                "<td>"+ msg[i].bet+ "</td>" +
-                "<td>"+ msg[i].cash_out+ "</td>" +
-                "<td>"+ msg[i].bonus+ "</td>" +
+                "<td>"+ bet + "</td>" +
+                "<td>"+ cash_out + "</td>" +
+                "<td>"+ bonus + "</td>" +
+                "<td>"+ margin + "</td>" +
                 "<td>"+ msg[i].cnt+ "</td>" +
                 "</tr>"
         }
+
+        html += "<tr><td colspan='4'>합계</td><td >"+margin_sum+"</td><td></td></tr>";
         list.html(html);
     });
 };
